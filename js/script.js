de9fad66facc225aa42d52948d52f947cc4cfb31
LeeVaughn <dlvprogramming@gmail.com>
2021-09-26 08:59:21 -0700

    Create initial function to transribe file

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ async function uploadFile(url="", data= {}) {
     mode: "cors",
     credentials: "same-origin",
     headers: {
-      authorization: "b62141f4d92b43f8a0f018ae6c8e018c",
+      authorization: apiKey,
       "content-type": "application/json",
       "Transfer-Encoding": "chunked" 
     },
@@ -13,8 +13,33 @@ async function uploadFile(url="", data= {}) {
   return response.json();
 }
 
-uploadFile("https://api.assemblyai.com/v2/upload", {audio_url: "/audio-files/peter-dawn.m4a"})
+async function transcribeFile(url="", data= {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    credentials: "same-origin",
+    headers: {
+      authorization: apiKey,
+      "content-type": "application/json",
+      "Transfer-Encoding": "chunked" 
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
+
+// uploadFile("https://api.assemblyai.com/v2/upload", {audio_url: "/audio-files/peter-dawn.m4a"})
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.error(err));
+
+transcribeFile("https://api.assemblyai.com/v2/transcript", {audio_url: "https://cdn.assemblyai.com/upload/4026f546-d110-4b35-b8f9-42c25ebdbab1"})
   .then(data => {
     console.log(data);
   })
   .catch((err) => console.error(err));
+
+document.getElementById("twisters").addEventListener("change", () => {
+  console.log("change");
+});
