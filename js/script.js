17edb49e676a143b140d566163928d84ceee6e2c
LeeVaughn <dlvprogramming@gmail.com>
2021-09-26 14:24:56 -0700

    Create initial variables, add comments to functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-async function uploadFile(url="", data= {}) {
-  const response = await fetch(url, {
-    method: "POST",
-    mode: "cors",
-    credentials: "same-origin",
-    headers: {
-      authorization: apiKey,
-      "Transfer-Encoding": "chunked" 
-    },
-    body: "binary"
-  });
-  return response.json();
-}
+const peterConstant = ["peter", "piper", "picked", "a", "peck", "of", "pickled", "peppers"];
+const sallyConstant = ["silly", "sally", "swiftly", "shooed", "seven", "silly", "sheep"];
+const woodchuckConstant = ["how", "much", "wood", "would", "a", "woodchuck", "chuck", "if", "a", "woodchuck", "could", "chuck", "wood"];
+const seashellsConstant = ["she", "sells", "seashells", "by", "the", "seashore"];
+// will be used to track which audio file was selected from the dropdown
+let selectedFile;
 
+
+/**
+ * Transcribes audio file
+ * 
+ * @param {string} url - the URL to use in the POST request
+ * @param {object} data - init object
+ * @return {object} response data in the form of a JSON object
+ */
 async function transcribeFile(url="", data= {}) {
   const response = await fetch(url, {
     method: "POST",
@@ -27,6 +28,13 @@ async function transcribeFile(url="", data= {}) {
   return response.json();
 }
 
+/**
+ * Get completed audio transcription
+ * 
+ * @param {string} url - the URL to use in the POST request
+ * @param {object} data - init object
+ * @return {object} response data in the form of a JSON object
+ */
 async function getTranscription(url="", data= {}) {
   const response = await fetch(url, {
     method: "GET",
@@ -39,12 +47,6 @@ async function getTranscription(url="", data= {}) {
   });
   return response.json();
 }
-
-// uploadFile("https://api.assemblyai.com/v2/upload", {audio_url: "https://github.com/LeeVaughn/tongue-twister-tester/blob/master/audio-files/peter-dawn.m4a?raw=true"})
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch((err) => console.error(err));
 
 // transcribeFile("https://api.assemblyai.com/v2/transcript", {audio_url: "https://github.com/LeeVaughn/tongue-twister-tester/blob/master/audio-files/peter-dawn.m4a?raw=true"})
 //   .then(data => {
